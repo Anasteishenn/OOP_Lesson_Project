e84bd1df75359ce9aba01dd0855f613055bd3d03
Anasteishenn <anastasisuvorova1@gmail.com>
2023-03-22 17:46:57 +0300

    Add methods to class Student (добавление поведения)

--- main.cpp
+++ main.cpp
@@ -8,9 +8,24 @@ public:
 	int age;
 	string surname;
 	float avr_mark;
+
+	void init(string n, string sname, int a, float mark) {
+
+		name = n;
+		surname = sname;
+		age = a;
+		avr_mark = mark;
+
+	}
+
+	string convert_to_string( ) {
+		return name + " " + surname + " (age- " + to_string(age)
+			+ ", mark- " + to_string(avr_mark) + ")";
+
+	}
 };
 
-void clin_student(Student st) {
+void clean_student(Student st) {
 	st.name = "no name";
 	st.surname = "no students";
 	st.age = 0;
@@ -18,49 +33,16 @@ void clin_student(Student st) {
 }
 
 
-string convert_to_string(Student st) {
-	return st.name + " " + st.surname + " (age-" + to_string (st.age)
-		+", mark-" + to_string (st.avr_mark) + ")";
-
-}
-
-
 int main() {
 	Student st1, st2, st3;
-	
-	st1.name = "Ivan";
-	st1.surname = "Ivanov";
-	st1.age = 14;
-	st1.avr_mark = 10;
 
-	st2.name = "Genrih";
-	st2.surname = "Gurshtinovich";
-	st2.age = 14;
-	st2.avr_mark = 9;
+	st1.init("Ivan", "Ivanov", 14, 10);
+	st2.init("Genrih", "Gurshtinovich", 14, 9);
+	st3.init("Tigran", "Sarkesyan", 14, 4);
 
-	st3.name = "Tigran";
-	st3.surname = "Sarkesyan";
-	st3.age = 14;
-	st3.avr_mark = 4;
-
-
-	cout << convert_to_string(st1) << endl;
-	cout << convert_to_string(st2) << endl;
-	cout << convert_to_string(st3) << endl;
-
-	/*Student st2 = st1;*/
-
-	/*st1.avr_mark = 9;*/
-
-	/*cout << "before funcsion calling:" << endl;
-	cout << st1.name << " " << st1.surname << " (age-" << st1.age << ", mark-" << st1.avr_mark<<")" << endl;
-
-	clin_student(st1);
-
-	cout << "after funcsion calling:" << endl;
-	cout << st1.name << " " << st1.surname << " (age-" << st1.age << ", mark-" << st1.avr_mark << ")" << endl;*/
-	/*cout << st1.name << " " << st2.surname << " (age- " << st2.age << ", mark-" << st2.avr_mark <<")" << endl;*/
-
+	cout << st1.convert_to_string ()<< endl;
+	cout << st2.convert_to_string() << endl;
+	cout << st3.convert_to_string() << endl;
 
 
 return 0;
